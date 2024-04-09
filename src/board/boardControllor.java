9fce2477df46f4b900f6e4ef4459817d56b9605d
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-04-10 04:06:18 +0700

    fix board page

--- src/board/boardControllor.java
+++ src/board/boardControllor.java
@@ -3,6 +3,7 @@ package board;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -15,23 +16,49 @@ import java.util.ResourceBundle;
 
 public class boardControllor  {
 
+    public ImageView rollDice;
     @FXML
-    private TilePane tile;
+    private TilePane tileBox;
 
     public void initialize() {
-        int numRectangles = 80; // Number of rectangles you want to create
+        rollDice.setImage(new Image("dice1.png"));
+//        int numRectangles = 81; // Number of rectangles you want to create
+        int tileSize = 77;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++){
+                Rectangle rectangle = new Rectangle();
 
-        for (int i = 0; i < numRectangles; i++) {
-            Rectangle rectangle = new Rectangle();
-            rectangle.setArcHeight(5.0);
-            rectangle.setArcWidth(5.0);
-            rectangle.setFill(Color.DODGERBLUE);
-            rectangle.setHeight(77.0);
-            rectangle.setWidth(77.0);
-            rectangle.setStroke(Color.BLACK);
-            rectangle.setStrokeType(StrokeType.INSIDE);
+                rectangle.setHeight(tileSize);
+                rectangle.setWidth(tileSize);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setStrokeType(StrokeType.INSIDE);
+                int tileNumber;
 
-            tile.getChildren().add(rectangle);
+                if (i % 2 == 0) {
+                    tileNumber = 82 - ((i * 9) + (j + 1));
+                } else  {
+                    tileNumber = 82 - (((i + 1) * 9) - j);
+                }
+
+                if (tileNumber % 2 == 0) {
+                    rectangle.setFill(Color.LIGHTGRAY);
+                } else {
+                    rectangle.setFill(Color.WHITE);
+                }
+                Text text = new Text(Integer.toString(tileNumber));
+                text.setX(j * tileSize + 30);
+                text.setY(i * tileSize + 20);
+                text.setTranslateZ(30);
+
+//                tileBox.getChildren().add(text);
+                tileBox.getChildren().add(rectangle);
+            }
+
+
+
+
+
+
         }
     }
 
